dbff3e7cdcc1b842d77cf31e3fe6d1441c9a5d98
Romenig Ribeiro <romenig@gmail.com>
2017-10-28 15:07:15 -0200

    Primeiro commit. Próximos trabalhos são para implementar a parte de intersecção.
    Signed-off-by: Romenig Ribeiro <romenig@gmail.com>

---
+++ js/application/action-manager.js
@@ -0,0 +1,355 @@
+var actionStack = [];
+var redoStack = [];
+
+function undo(){
+	var action = actionStack.pop();
+	action.undo();
+	redoStack.push(action);
+}
+
+function redo(){
+	var action = redoStack.pop();
+	action.execute();
+	actionStack.push(action);
+}
+
+//////////////////// ActionList ////////////////////////////
+
+//args: coordinates to the new point
+function createPoint(args){
+	var action = new CreatePoint();
+	action.setCoordinates(args.x, args.y);
+	action.execute();
+	actionStack.push(action);
+}
+
+//args: object containing two points
+function createSegment(args){
+	var action = new CreateSegment();
+	action.setPoints(args.pointA.x, args.pointA.y, args.pointB.x, args.pointB.y);
+	action.execute();
+	actionStack.push(action);
+}
+
+//args: two points
+function createLine(args){
+	var action = new CreateLine();
+	action.setPoints(args.pointA.x, args.pointA.y, args.pointB.x, args.pointB.y);
+	action.execute();
+	actionStack.push(action);
+}
+
+//args: two points
+function createRay(args){
+	var action = new CreateRay();
+	action.setPoints(args.pointA.x, args.pointA.y, args.pointB.x, args.pointB.y);
+	action.execute();
+	actionStack.push(action);
+}
+
+
+//args: two points
+function createCircunference(args){
+	var action = new CreateCircle();
+	action.setPoints(args.pointA.x, args.pointA.y, args.pointB.x, args.pointB.y);
+	action.setRadius(args.radius);
+	action.execute();
+	actionStack.push(action);
+}
+
+// CLASS DEFINITIONS ////////////////////////////////
+function CreatePoint(){
+	this.x = 0;
+	this.y = 0;
+	this.index = -1;
+	this.execute = function(){
+		if(this.point == null){
+			this.point = new paper.Path.Circle({
+    						center: new paper.Point(this.x, this.y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+			this.point.toString = "RegularPoint";
+		} else {
+			paper.project.activeLayer.insertChild(this.index, this.point);
+		}
+		paper.view.update();
+	}
+	this.setCoordinates = function(x, y){
+		this.x = x;
+		this.y = y;
+	}
+	this.undo = function(){
+		this.index = this.point.index;
+		this.point.remove();
+	}
+}
+
+function CreateSegment(){
+	this.pointA = null;
+	this.pointB = null;
+	this.segment = null;
+	this.indexA = -1;
+	this.indexB = -1;
+	this.indexS = -1;
+	this.execute = function(){
+		if(this.segment == null){
+			this.segment = new paper.Path.Line({
+	    						from: this.pointA.position,
+							    to: this.pointB.position,
+							    strokeColor: 'black'
+						 	});
+			this.segment.sendToBack();
+			this.segment.toString = "SegmentStroke";
+			this.segment.relatedObjects = [this.pointA, this.pointB];
+			this.pointA.relatedObjects.push(this.segment);
+			this.pointB.relatedObjects.push(this.segment);
+		}
+		else {
+			paper.project.activeLayer.insertChild(this.indexS, this.segment);
+			paper.project.activeLayer.insertChild(this.indexA, this.pointA);
+			paper.project.activeLayer.insertChild(this.indexB, this.pointB);
+		}
+		paper.view.update();
+	}
+	this.setPoints = function(a_x, a_y, b_x, b_y){
+		this.pointA = new paper.Path.Circle({
+    						center: new paper.Point(a_x, a_y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+		this.pointA.toString = "SegmentPoint";
+		this.pointB = new paper.Path.Circle({
+    						center: new paper.Point(b_x, b_y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+		this.pointB.toString = "SegmentPoint";
+		this.pointB.relatedObjects = [this.pointA];
+		this.pointA.relatedObjects = [this.pointB];
+	}
+	this.undo = function(){
+		this.indexA = this.pointA.index;
+		this.indexB = this.pointB.index;
+		this.indexS = this.segment.index;
+		this.pointA.remove();
+		this.pointB.remove();
+		this.segment.remove();
+	}
+}
+
+function CreateCircle(){
+
+	this.pointA = null;
+	this.pointB = null;
+	this.circunference = null;
+	this.radius = -1;
+	this.indexA = -1;
+	this.indexB = -1;
+	this.indexC = -1;
+
+	this.setPoints = function(a_x, a_y, b_x, b_y){
+		this.pointA = new paper.Path.Circle({
+    						center: new paper.Point(a_x, a_y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+		this.pointB = new paper.Path.Circle({
+    						center: new paper.Point(b_x, b_y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+		this.pointA.toString = "CircunferenceCenter";
+		this.pointB.toString = "CircunferenceRay";
+		this.pointA.relatedObjects = [this.pointB];
+		this.pointB.relatedObjects = [this.pointA];
+	}
+
+	this.setRadius = function(radius){
+		this.radius = radius;
+	}
+
+	this.execute = function(){
+		if(this.circunference == null){
+			this.circunference = new paper.Path.Circle({
+				center: this.pointA.position,
+				radius: this.radius,
+				strokeColor: 'black'
+			});
+			this.circunference.toString = "CircunferenceStroke";
+			this.circunference.relatedObjects = [this.pointA, this.pointB];
+			this.pointA.relatedObjects.push(this.circunference);
+			this.pointB.relatedObjects.push(this.circunference);
+
+		} else {
+			paper.project.activeLayer.insertChild(this.indexS, this.circunference);
+			paper.project.activeLayer.insertChild(this.indexA, this.pointA);
+			paper.project.activeLayer.insertChild(this.indexB, this.pointB);
+		}
+		paper.view.update();
+	}
+
+	this.undo = function(){
+		this.indexA = this.pointA.index;
+		this.indexB = this.pointB.index;
+		this.indexS = this.circunference.index;
+		this.circunference.remove();
+		this.pointA.remove();
+		this.pointB.remove();
+	}
+}
+
+
+function CreateLine(){
+	this.pointA = null;
+	this.pointB = null;
+	this.line = null;
+	this.indexA = -1;
+	this.indexB = -1;
+	this.indexS = -1;
+	this.execute = function(){
+		if(this.line == null){
+			var coeficienteAngular = coef(this.pointA.position, this.pointB.position);
+            var lineY0 = getLineY(this.pointA.position, 0, coeficienteAngular);
+            var lineYwidth = getLineY(this.pointA.position, $("#canvas").width(), coeficienteAngular);
+			this.line = new paper.Path.Line({
+	    						from: new paper.Point(0,lineY0),
+							    to: new paper.Point($("#canvas").width(), lineYwidth),
+							    strokeColor: 'black'
+						 	});
+			this.line.toString = "LineStroke";
+			this.line.relatedObjects = [this.pointA, this.pointB];
+			this.pointA.relatedObjects.push(this.line);
+			this.pointB.relatedObjects.push(this.line);
+			this.line.sendToBack();
+		}
+		else {
+			paper.project.activeLayer.insertChild(this.indexS, this.line);
+			paper.project.activeLayer.insertChild(this.indexA, this.pointA);
+			paper.project.activeLayer.insertChild(this.indexB, this.pointB);
+		}
+		paper.view.update();
+	}
+	this.setPoints = function(a_x, a_y, b_x, b_y){
+		this.pointA = new paper.Path.Circle({
+    						center: new paper.Point(a_x, a_y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+		this.pointA.toString = "LinePoint";
+		this.pointB = new paper.Path.Circle({
+    						center: new paper.Point(b_x, b_y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+		this.pointB.toString = "LinePoint";
+		this.pointB.relatedObjects = [this.pointA];
+		this.pointA.relatedObjects = [this.pointB];
+	}
+	this.undo = function(){
+		this.indexA = this.pointA.index;
+		this.indexB = this.pointB.index;
+		this.indexS = this.line.index;
+		this.pointA.remove();
+		this.pointB.remove();
+		this.line.remove();
+	}
+}
+
+function CreateRay(){
+	this.pointA = null;
+	this.pointB = null;
+	this.line = null;
+	this.indexA = -1;
+	this.indexB = -1;
+	this.indexS = -1;
+	this.execute = function(){
+		if(this.line == null){
+			var coeficienteAngular = coef(this.pointA.position, this.pointB.position);
+            var lineYwidth = getLineY(this.pointA.position, $("#canvas").width(), coeficienteAngular);
+            var lineYwidth;
+            var toPoint;
+            if(this.pointB.position.x <= this.pointA.position.x){
+                lineYwidth = getLineY(this.pointA.position, 0, coeficienteAngular);
+                toPoint = new paper.Point(0, lineYwidth);
+            } else{
+                lineYwidth = getLineY(this.pointA.position, $("#canvas").width(), coeficienteAngular);
+               	toPoint = new paper.Point($("#canvas").width(), lineYwidth);
+            }
+			this.line = new paper.Path.Line({
+	    						from: this.pointA.position,
+							    to: toPoint,
+							    strokeColor: 'black'
+						 	});
+			this.line.sendToBack();
+			this.line.toString = "RayStroke";
+			this.pointA.relatedObjects.push(this.pointB);
+			this.pointB.relatedObjects.push(this.pointA);
+			this.line.relatedObjects = [this.pointA, this.pointB];
+		}
+		else {
+			paper.project.activeLayer.insertChild(this.indexS, this.line);
+			paper.project.activeLayer.insertChild(this.indexA, this.pointA);
+			paper.project.activeLayer.insertChild(this.indexB, this.pointB);
+		}
+		paper.view.update();
+	}
+	this.setPoints = function(a_x, a_y, b_x, b_y){
+		this.pointA = new paper.Path.Circle({
+    						center: new paper.Point(a_x, a_y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+		this.pointA.toString = "RayPoint";
+		this.pointB = new paper.Path.Circle({
+    						center: new paper.Point(b_x, b_y),
+    						radius: 4,
+    						strokeColor: 'black',
+    						fillColor: 'blue'
+						 });
+		this.pointB.toString = "RayPoint";
+		this.pointA.relatedObjects = [this.pointB];
+		this.pointB.relatedObjects = [this.pointA];
+	}
+	this.undo = function(){
+		this.indexA = this.pointA.index;
+		this.indexB = this.pointB.index;
+		this.indexS = this.line.index;
+		this.pointA.remove();
+		this.pointB.remove();
+		this.line.remove();
+	}
+
+}
+
+
+function coef(pontoA, pontoB){
+    var coef = (pontoA.y - pontoB.y)/(pontoA.x - pontoB.x);
+    return coef;
+}
+
+function getLineY(ponto, x, coef){
+    var y = coef * (x - ponto.x) + ponto.y;
+    return y;
+}
+
+//---------------------------------------
+
+function showIntersections(path1, path2) {
+    var intersections = getProjectIntersections();
+    for (var i = 0; i < intersections.length; i++) {
+        new Path.Circle({
+            center: intersections[i].point,
+            radius: 5,
+            fillColor: '#009dec'
+        }).removeOnMove();
+    }
+}
---
+++ js/application/mouse.js
@@ -0,0 +1,274 @@
+var values = {
+    paths: 50,
+    minPoints: 5,
+    maxPoints: 15,
+    minRadius: 30,
+    maxRadius: 90
+};
+
+var hitOptions = {
+    segments: true,
+    stroke: true,
+    fill: true,
+    tolerance: 5
+};
+
+var segment, path;
+var movePath = false;
+
+var isConstructing = true;
+var point1;
+var point2;
+var tempPath;
+
+
+
+function mousedown(event, currentBtn){
+    switch(currentBtn) {
+    case 'selection':
+    console.log("C");
+        testHitOnProject(event);
+        break;
+    case 'point':
+        createPoint(event.point);
+        break;
+    case 'segment':
+        if(isConstructing){
+            // cria o ponto e começa a rastrear o movimento
+            initConstruction(event.point);
+            isConstructing = false;
+        } else {
+            createSegment({pointA: point1.position, pointB: point2.position});
+            point1.remove();
+            point2.remove();
+            tempPath.remove();
+            point1 = point2 = null;
+            tempPath = null;
+            isConstructing = true;
+        }
+        break;
+    case 'circunference':
+        if(isConstructing){
+            initConstruction(event.point);
+            isConstructing = false;
+        }else{
+            createCircunference({pointA: point1.position, pointB: point2.position, radius: distanceBetween2points(point1.position, point2.position)})
+            point1.remove();
+            point2.remove();
+            tempPath.remove();
+            point1 = point2 = null;
+            tempPath = null;
+            isConstructing = true;
+        }
+        break;
+    case 'line':
+        if(isConstructing){
+            initConstruction(event.point);
+            isConstructing = false;
+        }else{
+            createLine({pointA: point1.position, pointB: point2.position});
+            point1.remove();
+            point2.remove();
+            tempPath.remove();
+            point1 = point2 = null;
+            tempPath = null;
+            isConstructing = true;
+        }
+        break;
+        case 'ray':
+        if(isConstructing){
+            initConstruction(event.point);
+            isConstructing = false;
+        }else{
+            createRay({pointA: point1.position, pointB: point2.position});
+            point1.remove();
+            point2.remove();
+            tempPath.remove();
+            point1 = point2 = null;
+            tempPath = null;
+            isConstructing = true;
+        }
+        break;
+    default:
+        console.log("default switch mousedown");
+    }
+}
+
+function mousemove(event){
+    if(!window.mobileAndTabletcheck()){
+        moveOrDrag(event);
+    }
+}
+
+function mousedrag(event){
+    if(window.mobileAndTabletcheck()){
+        moveOrDrag(event);
+    }
+}
+
+function moveOrDrag(event){
+    if(!isConstructing && currentBtn == 'segment'){
+        if(point2 == null){
+            point2 = new paper.Path.Circle({
+                            center: new paper.Point(this.x, this.y),
+                            radius: 4,
+                            strokeColor: 'black',
+                            fillColor: 'blue'
+                         });
+            tempPath = new paper.Path.Line({
+                                from: point1.position,
+                                to: point2.position,
+                                strokeColor: 'black'
+                            });
+        } else {
+            point2.position = event.point;
+            tempPath.segments[1].point = event.point;
+        }
+    } else if (!isConstructing && currentBtn == 'circunference') {
+        if(point2 == null){
+            point2 = new paper.Path.Circle({
+                            center: new paper.Point(this.x, this.y),
+                            radius: 4,
+                            strokeColor: 'black',
+                            fillColor: 'blue'
+                         });    
+            tempPath = new paper.Path.Circle({
+                            center: point1.position,
+                            radius: distanceBetween2points(point1.position, point2.position),
+                            strokeColor: 'black'
+                         }); 
+        }else{
+            point2.position = event.point;
+            tempPath.remove();
+            tempPath = new paper.Path.Circle({
+                            center: point1.position,
+                            radius: distanceBetween2points(point1.position, point2.position),
+                            strokeColor: 'black'
+                         }); 
+            
+        }
+    } else if(!isConstructing && currentBtn == 'line'){
+        if(point2 == null){
+            point2 = new paper.Path.Circle({
+                            center: new paper.Point(this.x, this.y),
+                            radius: 4,
+                            strokeColor: 'black',
+                            fillColor: 'blue'
+                         });
+            tempPath = new paper.Path.Line({
+                                from: point1.position,
+                                to: point2.position,
+                                strokeColor: 'black'
+                            });
+        } else {
+            point2.position = event.point;
+            var coeficienteAngular = coef(point1.position, event.point);
+            var lineY0 = getLineY(point1.position, 0, coeficienteAngular);
+            var lineYwidth = getLineY(point1.position, $("#canvas").width(), coeficienteAngular);
+            tempPath.segments[0].point = new paper.Point(0,lineY0);
+            tempPath.segments[1].point = new paper.Point($("#canvas").width(), lineYwidth);
+        }
+    } else if(!isConstructing && currentBtn == 'ray'){
+        if(point2 == null){
+            point2 = new paper.Path.Circle({
+                            center: new paper.Point(this.x, this.y),
+                            radius: 4,
+                            strokeColor: 'black',
+                            fillColor: 'blue'
+                         });
+            tempPath = new paper.Path.Line({
+                                from: point1.position,
+                                to: point2.position,
+                                strokeColor: 'black'
+                            });
+        } else {
+            point2.position = event.point;
+
+
+            var coeficienteAngular = coef(point1.position, event.point);
+            var lineYwidth;
+            if(point2.position.x <= point1.position.x){
+                lineYwidth = getLineY(point1.position, 0, coeficienteAngular);
+                tempPath.segments[1].point = new paper.Point(0, lineYwidth);
+            } else{
+                lineYwidth = getLineY(point1.position, $("#canvas").width(), coeficienteAngular);
+                tempPath.segments[1].point = new paper.Point($("#canvas").width(), lineYwidth);
+            }
+        }
+    } else if(isConstructing && currentBtn == 'selection'){
+        project.activeLayer.selected = false;
+        highlightMethod(event);
+    }
+}
+
+
+function distanceBetween2points(pointA, pointB){
+    return Math.sqrt(Math.pow((pointA.x - pointB.x),2)+Math.pow((pointA.y - pointB.y),2))
+}
+
+function initConstruction(point){
+    point1 = new paper.Path.Circle({
+                            center: new paper.Point(point.x, point.y),
+                            radius: 4,
+                            strokeColor: 'black',
+                            fillColor: 'blue'
+                         });
+}
+
+var segmentHitResult, pathHitResult;
+var movepathHitResult = false;
+var selectedObjects = [];
+
+function testHitOnProject(event){
+    segmentHitResult = pathHitResult = null;
+    var hitResult = project.hitTestAll(event.point, hitOptions);
+    if (hitResult.length == 0 || hitResult[0].isClone){
+        project.activeLayer.selected = false;
+        return;
+    } else {
+        for(var i = 0; i < hitResult.length; i++){
+            createShadow(hitResult[i].item);
+            selectedObjects.push(hitResult[i].item);
+            if(hitResult[i].item.toString != "RegularPoint"){
+                createShadow(hitResult[i].item.relatedObjects[0]);
+                selectedObjects.push(hitResult[i].item.relatedObjects[0]);
+                createShadow(hitResult[i].item.relatedObjects[1]);
+                selectedObjects.push(hitResult[i].item.relatedObjects[1]);
+            }    
+        }
+    }   
+}
+
+function createShadow(item){
+    var clone = item.clone();
+    clone.strokeWidth = 10;
+    clone.strokeColor = new paper.Color(0.7,0.7,1,0.4);
+    clone.sendToBack();
+    clone.isClone = true;
+    item.shadowClone = clone;
+}
+
+function highlightMethod(event){
+    segmentHitResult = pathHitResult = null;
+    var hitResult = project.hitTestAll(event.point, hitOptions);
+
+    if (hitResult.length == 0 || hitResult[0].item.isClone){
+        console.log("entrou no if");
+        project.activeLayer.selected = false;
+        return;
+    } else {
+        for(var i = 0; i < hitResult.length; i++){
+            if(!hitResult[i].item.isClone){
+                hitResult[i].item.selected = true;
+                if(hitResult[i].item.toString != "RegularPoint"){
+                    console.log(hitResult[i].item.isClone);
+                    hitResult[i].item.relatedObjects[0].selected = true;
+                    hitResult[i].item.relatedObjects[1].selected = true;
+                }    
+
+            }
+        }
+    }   
+
+}
+
